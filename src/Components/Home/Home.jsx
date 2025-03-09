ed8dd65c9c774950b62e0afd89ce139faf56f116
OmarKh22 <ok597451@gmail.com>
2025-03-09 02:11:38 +0200

    HeroSectionUpdade

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './Home.module.css';
-import podcastHome from "../../Assets/images/home/podcastHome.png";
-import HomeImg from "../../Assets/images/home/HomeImg.jpg";
 import Podcasts from '../Podcasts/Podcasts';
 
 const Home = () => {
@@ -9,49 +7,35 @@ const Home = () => {
     <>
       <section className={styles.homePage}>
         <main className={`container ${styles.container}`}>
-          <section
-            id="hero"
-            className={`row align-items-center ${styles.hero}`}
-          >
-            <div className="col-lg-6 col-md-12 text-center text-lg-start">
-              <div className={styles.heroContent}>
-                <img
-                  src={podcastHome}
-                  alt="Podcast"
-                  className={styles.heroTitle}
-                />
-                <p className={styles.heroText}>
-                  منصةإلهام حيث يلتقي الإلهام بالإبداع لطالما أُحطنا بشخصيات
-                  ملهمة، تركت بصماتها في مختلف المجالات، لكن بعضها ظلت خلف
-                  الكواليس بعيدًا عن الأضواء. أشخاص يملكون قصصًا تستحق أن تُروى،
-                  وتجارب مليئة بالدروس، لكن لم تتح لنا فرصة التعرف عليهم عن قرب
-                  أو الاستفادة من رحلاتهم الملهمة. من هنا، وُلدت منصة إلهام،
-                  برؤية واضحة ورسالة سامية: دعم الملهمين. نؤمن أن لكل شخص قصة
-                  تستحق أن تُسمع، ولكل تجربة قيمة يمكن أن تكون شرارة إلهام
-                  لغيره. لذا، قررنا أن نسلط الضوء على هذه الشخصيات، ونعطيها
-                  المساحة التي تستحقها، لنكون جسرًا بين الإلهام وأولئك الذين
-                  يبحثون عنه.
-                </p>
-                <div className={`${styles.heroBtnGroup}`}>
-                  <button className={`btn btn-primary ${styles.btn}`}>
-                    <span>Listen Now</span>
-                  </button>
+          <section className={`${styles.homePage}`}>
+            <main className={`container ${styles.container}`}>
+              <section id="hero" className={`row ${styles.hero}`}>
+                <div className="col-12">
+                  <div className={styles.heroContent}>
+                    <p
+                      className={`${styles.heroText} text-center`}
+                    >
+                      منصة إلهام حيث يلتقي الإلهام بالإبداع لطالما أُحطنا بشخصيات
+                      ملهمة، تركت بصماتها في مختلف المجالات، لكن بعضها ظلت خلف
+                      الكواليس بعيدًا عن الأضواء. أشخاص يملكون قصصًا تستحق أن
+                      تُروى، وتجارب مليئة بالدروس، لكن لم تتح لنا فرصة التعرف
+                      عليهم عن قرب أو الاستفادة من رحلاتهم الملهمة. من هنا،
+                      وُلدت منصة إلهام، برؤية واضحة ورسالة سامية: دعم الملهمين.
+                      نؤمن أن لكل شخص قصة تستحق أن تُسمع، ولكل تجربة قيمة يمكن
+                      أن تكون شرارة إلهام لغيره. لذا، قررنا أن نسلط الضوء على
+                      هذه الشخصيات، ونعطيها المساحة التي تستحقها، لنكون جسرًا
+                      بين الإلهام وأولئك الذين يبحثون عنه. <br /> منصة إلهام.. حيث
+                      يلتقي الإلهام بالإبداع، وتُروى القصص التي تستحق أن تُسمع..
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </div>
-
-            <div className="col-lg-6 col-md-12 d-flex justify-content-center">
-              <div
-                className={styles.heroBanner}
-                role="img"
-                aria-label="Hero banner"
-              >
-                <img src={HomeImg} alt="Podcast Banner" loading="lazy" />
-              </div>
-            </div>
+              </section>
+            </main>
           </section>
         </main>
       </section>
+
+
       <Podcasts />
     </>
   );
